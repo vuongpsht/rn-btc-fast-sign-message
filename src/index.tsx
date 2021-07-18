b8c848aff55b739f39a6588ce88dbcc472bf45f7
vuongpsht <Hoang-thien-vuong.phu-sanh@ant-tech.eu>
2021-07-18 23:58:30 +0700

    typing

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import { NativeModules } from 'react-native';
 
+interface WalletInfo {
+  address: string,
+  mnemonics: string,
+  privateKey: string,
+  publicKey: string
+}
+
 type BitcoinType = {
   multiply(a: number, b: number): Promise<number>;
-  signMessage(): Promise<string>;
-  generateMnemonic(length: number): Promise<string>;
-  mnemonicToWallet(mnemonic: string, path: string): Promise<null>;
+  // signMessage(): Promise<string>;
+  generateMnemonic(length: number): Promise<WalletInfo>;
+  mnemonicToWallet(mnemonic: string, path: string): Promise<WalletInfo>;
   // signMessage(msg: string, privateKey: string): Promise<string>;
 };
 
